7ccdfc78333d75cfd11de337b0e61135f3a8a5ec
Douglas Donohue <falloutelite@gmail.com>
2019-02-20 19:29:55 +0000

    jasmine isnt working lmao

--- spec/jsSpec.js
+++ spec/jsSpec.js
@@ -2,7 +2,7 @@
 describe('Check Sequence function', function() {
     it('passes if arrays are equal', function() {
         var a = [1, 2];
-        var b = [1, 2];
+        var b = [1, 3];
          expect(checkSequence(a, b)).toBe(true);
     }); 
 });
