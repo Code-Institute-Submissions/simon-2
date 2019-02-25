d084e8040becc090c64b1560ad1319411faad7fc
Douglas Donohue <falloutelite@gmail.com>
2019-02-25 20:08:52 +0000

    changed js.js to app.js

--- spec/jsSpec.js
+++ spec/jsSpec.js
@@ -3,6 +3,6 @@ describe('Check Sequence function', function() {
     it('passes if arrays are equal', function() {
         var a = [1, 2];
         var b = [1, 2];
-         expect(checkSequence(a, b)).toBe(true);
+        expect(checkSequence(a, b)).toBe(true);
     }); 
 });
